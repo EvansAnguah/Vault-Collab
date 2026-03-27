9b9687acc920bcea227f814c1426f18f16de476b
EvansAnguah <evansobeng022@gmail.com>
2026-03-27 15:11:25 +0000

    Fix autoloader class conflict and add PHPMailer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rmu/vault-collab',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0c6ea6eea02933355077218bf02f3b4f618be424',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rmu/vault-collab' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0c6ea6eea02933355077218bf02f3b4f618be424',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
